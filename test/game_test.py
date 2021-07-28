66079149f6575167adf7c9dc297d999c9984d3bd
MJ Rossetti <s2t2@users.noreply.github.com>
2021-07-27 21:05:48 -0400

    Update game to use tested function

--- test/game_test.py
+++ test/game_test.py
@@ -1,4 +1,19 @@
 
-# TODO: import some code
+
+from app.game import determine_winner
 
 # TODO: test the code
+
+#u = "rock"
+#c = "scissors"
+#winner = "USER" # / rock
+
+def test_enlarge():
+    assert determine_winner(user_choice="rock", computer_choice="paper") == "paper"
+    assert determine_winner(user_choice="rock", computer_choice="rock") == None
+    assert determine_winner(user_choice="rock", computer_choice="scissors") == "rock"
+
+    #assert determine_winner(user_choice="rock", computer_choice="paper") == "paper"
+    #assert determine_winner(user_choice="rock", computer_choice="paper") == "paper"
+    #assert determine_winner(user_choice="rock", computer_choice="paper") == "paper"
+    #assert determine_winner(user_choice="rock", computer_choice="paper") == "paper"
